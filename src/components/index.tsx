aea871c20db8886dfe0ffed109f4681efe14a9a2
homfarnam <homfarnam@gmail.com>
2021-07-20 14:06:31 +0430

    Create components imports

---
+++ src/components/index.tsx
@@ -0,0 +1,7 @@
+import Title from "./Title";
+import Image from "./Image";
+import TableData from "./TableData";
+import Layout from "./Layout";
+import Footer from "./Footer";
+
+export { Title, Image, TableData, Layout, Footer };
