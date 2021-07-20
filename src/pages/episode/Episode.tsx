c34b6c83d46f6be407e42d33fefb0d2b992f6b0e
homfarnam <homfarnam@gmail.com>
2021-07-20 14:06:39 +0430

    Create episode page

---
+++ src/pages/episode/Episode.tsx
@@ -0,0 +1,55 @@
+import axios from 'axios';
+import { Image, Layout, Title } from 'components';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import { Router, useParams } from 'react-router';
+
+type ShowTypes = {
+  name: string;
+  summary: any;
+  image: {
+    medium: string;
+    original: string;
+  };
+  _links: {
+    previousepisode: {
+      href: string;
+    };
+  };
+};
+
+export const Episode = () => {
+  const [episode, setEpisode] = useState<ShowTypes>();
+
+  const { id }: any = useParams();
+  console.log('id: ', id);
+
+  const getEpisode = (epId: number) => {
+    axios
+      .get(`https://api.tvmaze.com/episodes/${epId}`)
+      .then((res) => res)
+      .then((res) => {
+        setEpisode(res?.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    getEpisode(id);
+  }, [id]);
+
+  return (
+    <Layout>
+      <div className="w-full h-screen">
+        <Title name={episode?.name as string} />
+
+        <Image
+          url={episode?.image.medium as string}
+          alt={episode?.name as string}
+        />
+      </div>
+    </Layout>
+  );
+};
+
+export default Episode;
