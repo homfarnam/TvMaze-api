e02875b6a8f060f600d349f490fb18e44352ad82
homfarnam <homfarnam@gmail.com>
2021-07-23 01:23:26 +0430

    Create episode reducer

---
+++ src/redux/episode/episode.ts
@@ -0,0 +1,73 @@
+import {
+  createAsyncThunk,
+  createSlice,
+  Slice,
+  SliceCaseReducers
+} from '@reduxjs/toolkit';
+// import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+// import { Reducer } from 'react';
+
+export const getEpisode: any = createAsyncThunk(
+  'episodesData',
+  async (epId) => {
+    return fetch(`https://api.tvmaze.com/episodes/${epId}`)
+      .then((res) => res.json())
+      .catch((err) => console.log(err));
+  }
+);
+
+interface ShowTypes {
+  episode: {
+    name: string;
+    summary: any;
+    image: {
+      medium: string;
+      original: string;
+    };
+    _links: {
+      previousepisode: {
+        href: string;
+      };
+    };
+  }[];
+  loading: boolean;
+  hasErrors: boolean;
+}
+
+const initialState: ShowTypes = {
+  episode: [],
+  loading: false,
+  hasErrors: false
+};
+
+export const episode: Slice<
+  ShowTypes,
+  SliceCaseReducers<ShowTypes>,
+  'episodeData'
+> = createSlice({
+  name: 'episodeData',
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [getEpisode.pending]: (state, action) => {
+      state.loading = true;
+    },
+    [getEpisode.fulfilled]: (state, action) => {
+      state.episode = action.payload;
+      state.loading = false;
+      state.hasErrors = false;
+    },
+    [getEpisode.rejected]: (state, { payload }) => {
+      state.loading = false;
+      state.hasErrors = true;
+    }
+  }
+});
+
+export const episodeActions = episode.actions;
+
+export const episodesSelector = (state: ShowTypes) => state;
+
+export default episode.reducer;
+
+// export default episodesSlice.reducer;
